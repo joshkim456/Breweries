37a82d607e63d1b53d9f10e1ac34d9386781f14f
Josh Kim <kim.j@etoncollege.org.uk>
2024-06-11 09:55:53 +0100

    Initial Commit

---
+++ Breweries/BreweriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreweriesApp.swift
+//  Breweries
+//
+//  Created by Kim, Joshua (WING) on 11/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct BreweriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
